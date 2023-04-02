ed12aa1532cb2068b931bc47d2b8ec6a77d9be03
Stevepetoskey <41028855+Stevepetoskey@users.noreply.github.com>
2023-04-02 13:54:07 -0400

    Delete scripts directory

--- scripts/Bars.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-onready var player = get_node("../../../Player")
-
-func _process(delta):
-	$Health.max_value = player.maxHealth
-	$Health.value = player.health
-	$Oxygen.max_value = player.maxOxygen
-	$Oxygen.value = player.oxygen
